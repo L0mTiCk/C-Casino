dcea8580719b9eb444e408d648fa2a82b1d0748b
Aleksey <100795000+L0mTiCk@users.noreply.github.com>
2022-09-21 21:44:37 +0300

    Add Mian.cpp

---
+++ Main/Main.cpp
@@ -0,0 +1,39 @@
+﻿// Main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	ifstream fin("stat.txt");
+	if (!fin.is_open())
+	{
+		ofstream fout("stat.txt");
+		fout.close();
+		cout << "Restart programm!";
+	}
+	else {
+		vector <string> name;
+		vector <int> score;
+		vector <string> password;
+		while (!fin.eof())
+		{
+			string name, password;
+			int score = 0;
+			fin >> name >> password >> score;
+			cout << name << ' ' << password << ' ' << score << endl;
+		}
+	}
+}
+
